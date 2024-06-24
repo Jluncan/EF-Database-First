e1b10dba5c84df8f8b92254d50b9f34f2cb57af2
James Luncan <jluncan@tql.com>
2024-06-24 16:30:40 -0400

    Add project files.

---
+++ EF-Database-First/Program.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace EF_Database_First {
+    internal class Program {
+        static void Main(string[] args) {
+
+            var _context = new TqlprsdbContext();
+
+           // var newRequest = from r in _context.Requests
+           //                  join rl in _context.Requestlines
+           //                  on r.Id equals rl.RequestId
+           //                  join p in _context.Products
+            //                 on rl.ProductId equals p.Id
+           //                  join v in _context.Vendors
+           //                  on p.VendorId equals v.Id
+           //                   join u in _context.Users
+                                
+
+            // var newUser = new User {
+            //     Id= 0,Username = "JC", Password = "JC" , Firstname = "James" , Lastname = "Luncan" ,
+            //     IsReviewer = true, IsAdmin = true, 
+
+            //   };
+
+            //  _context.Users.Add(newUser);
+
+            // _context.SaveChanges();
+
+            //  var deleteUser = _context.Users.Find(4);
+
+            //  _context.Users.Remove(deleteUser!);
+
+            //  _context.SaveChanges();
+
+
+            var newVendor = new Vendor {
+                Id = 0 , Code = "Jc" , Name = "James", Address = "123 Jc Street" , 
+                City = "Cincinnati", State = "OH" , Zip = "45040"
+
+                
+
+            };
+            _context.Vendors.Add(newVendor);
+
+            _context.SaveChanges();
+
+            var newProduct = new Product {
+                Id = 0, PartNbr = "Rolex", Name = "Rolex", Price = 50000, Unit = "EacH", VendorId = 3
+
+            };
+            _context.Products.Add(newProduct);
+
+            _context.SaveChanges();
+
+            var newRequest = new Request {
+                Id= 0,Description = "I Want a Rolex" , Justification = " Because" , DeliveryMode = "Pickup",
+                Status = "Used" , Total = 0, UserId = 3
+
+            };
+            _context.Requests.Add(newRequest);
+
+            _context.SaveChanges();
+
+            var newRequestLine2 = new Requestline {
+                Id = 0, Quantity = 1, RequestId = 2, ProductId = 1
+
+            };
+            _context.Requestlines.Add(newRequestLine2);
+
+            _context.SaveChanges();
+        }
+    }
+}
